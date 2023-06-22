19101376f9dc00f5d43454508eeb1551aa2e8c46
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-06-22 11:57:51 +0400

    add solutions in lists.py

--- lists/lists.py
+++ lists/lists.py
@@ -1,23 +1,33 @@
 class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
-        """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
+        if not input_list:
+            return input_list
 
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
-        """
-        pass
+        max_value = input_list[0]
+        for num in input_list:
+            if num > max_value:
+                max_value = num
+
+        i = 0
+        while i < len(input_list):
+            if input_list[i] > 0:
+                input_list[i] = max_value
+            i += 1
+        return input_list
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
 
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
-        """
-        pass
+        low = 0
+        high = len(input_list) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if input_list[mid] == query:
+                return mid
+            if input_list[mid] > query:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
