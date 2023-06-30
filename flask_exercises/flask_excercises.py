02f51a2421d031fc1df61f12cb34d4578390c9dc
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-06-30 12:27:13 +0400

    add solutions in flask_excercises

--- flask_exercises/flask_excercises.py
+++ flask_exercises/flask_excercises.py
@@ -1,31 +1,58 @@
-from flask import Flask
+from flask import Flask, request
 
 
 class FlaskExercise:
-    """
-    Вы должны создать API для обработки CRUD запросов.
-    В данной задаче все пользователи хранятся в одном словаре, где ключ - это имя пользователя,
-    а значение - его параметры. {"user1": {"age": 33}, "user2": {"age": 20}}
-    Словарь (dict) хранить в памяти, он должен быть пустым при старте flask.
-
-    POST /user - создание пользователя.
-    В теле запроса приходит JSON в формате {"name": <имя пользователя>}.
-    Ответ должен вернуться так же в JSON в формате {"data": "User <имя пользователя> is created!"}
-    со статусом 201.
-    Если в теле запроса не было ключа "name", то в ответ возвращается JSON
-    {"errors": {"name": "This field is required"}} со статусом 422
-
-    GET /user/<name> - чтение пользователя
-    В ответе должен вернуться JSON {"data": "My name is <name>"}. Статус 200
-
-    PATCH /user/<name> - обновление пользователя
-    В теле запроса приходит JSON в формате {"name": <new_name>}.
-    В ответе должен вернуться JSON {"data": "My name is <new_name>"}. Статус 200
-
-    DELETE /user/<name> - удаление пользователя
-    В ответ должен вернуться статус 204
-    """
-
     @staticmethod
     def configure_routes(app: Flask) -> None:
-        pass
+        users_data = {}
+
+        def not_found():
+            return {"errors": {"name": "User not found"}}, 404
+
+        def wrong_content_type():
+            return {"errors": {"content_type": "Wrong content type"}}, 415
+
+        @app.route("/user", methods=["POST"])
+        def user_create():
+            content_type = request.headers.get("Content-Type")
+            if content_type == "application/json":
+                user = request.json
+                if "name" in user:
+                    users_data.update(user)
+                    new_user = {"data": f"User {user['name']} is created!"}
+                    return new_user, 201
+                else:
+                    error = {"errors": {"name": "This field is required"}}
+                    return error, 422
+            else:
+                return wrong_content_type()
+
+        @app.route("/user/<name>", methods=["GET"])
+        def user_name(name):
+            if "name" in users_data:
+                getted_name = {"data": f"My name is {users_data['name']}"}
+                return getted_name, 200
+            else:
+                return not_found()
+
+        @app.route("/user/<name>", methods=["PATCH"])
+        def update_user(name):
+            content_type = request.headers.get("Content-Type")
+            if content_type == "application/json":
+                user = request.json
+                if "name" in users_data:
+                    users_data.update(user)
+                    updated_user = {"data": f"My name is {user['name']}"}
+                    return updated_user, 200
+                else:
+                    return not_found()
+            else:
+                return wrong_content_type()
+
+        @app.route("/user/<name>", methods=["DELETE"])
+        def delete_user(name):
+            if "name" in users_data:
+                del users_data["name"]
+                return "", 204
+            else:
+                return not_found()
